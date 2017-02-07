12c1c8231c5ec08e1200b112b98005e4d0779ae9
Kezia <kezia@kobana.com>
2017-02-07 22:23:46 +0100

    devoir de maison commentaires

--- EXO/dm.py
+++ EXO/dm.py
@@ -1,11 +1,12 @@
+## les fonctions
 def affiche (c):
     for k in range(len(c[0])):
         print(c[k])
 
 def sommeligne(C,i):
     """ cette fonction renvoie la somme des valeurs de la ligne i de C"""
-    a=0
-    b=len(C[0])
+    a = 0
+    b = len(C[0])
     for k in range(b):
         a = C[i][k]+a
         print(a)
