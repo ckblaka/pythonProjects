c09ca1ad4cc972d72dac49de707c45af7ad50cd4
ckblaka <constant@kobana.com>
2017-02-10 13:48:09 +0100

    Somme de la 2eme diagonale

--- EXO/dm.py
+++ EXO/dm.py
@@ -3,6 +3,17 @@ def affiche (c):
     for k in range(len(c)):
         print(c[k])
 
+def sommeDiag2(t):
+    affiche(t)
+    n = len(t)
+    j = 0
+    somme = 0
+    for i in range(n - 1, -1, -1):
+        print(i)
+        somme = somme + t[j][i]
+        j = j + 1
+    return somme
+
 def sommeligne(c,i):
     """ cette fonction renvoie la somme des valeurs de la ligne i de C"""
     a = 0
